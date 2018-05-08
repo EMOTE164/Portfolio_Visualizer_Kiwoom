931583576e384246f8ea6e6f570be3adbb8a06a2
NEPS <ldgeao99@naver.com>
2018-05-08 21:48:25 +0900

    같은 기능을 직관적인 호출 구조로 보기쉽게 수정함

--- PostgreSQL_code/postgreSQL_AddDailyPrice.py
+++ PostgreSQL_code/postgreSQL_AddDailyPrice.py
@@ -1,8 +1,7 @@
-# opt10081을 이용해 stock_kospi.daily_price, stock_kosdaq.daily_price 테이블을 채움
-
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
 import time
 import psycopg2
 
@@ -13,114 +12,155 @@ password = "emote164"   #설치할 때 지정한 것
 port = "9003"           #설치할 때 지정할 것
 
 conn_string = "host={0} user={1} dbname={2} password={3} port={4}".format(host, user, dbname, password, port)
+MODE = "kospi" # kosdaq으로 지정하면 코드닥에 대한 데이터도 처리가능하다.
 
-class Program(QMainWindow):
+TR_REQ_TIME_INTERVAL = 0.2
 
+class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.kiwoom.dynamicCall("CommConnect()")  # 로그인창 호출
-        # 서버로부터 오는 이벤트에 콜백함수 등록
-        self.kiwoom.OnEventConnect.connect(self.receiveLoginEvent)
-        self.kiwoom.OnReceiveTrData.connect(self.receiveTrData)
-        self.code_name_list = []
-        self.code_name_list_currentIndex = 0
+        self._create_kiwoom_instance()
+        self._set_signal_slots()
+        self.req_stock_code = ""    #실행중에는 현재 작업중인 주식코드를 저장하고 있음.
 
-    # 로그인창에서 아이디와 패스워드가 맞으면 불리는 함수
-    def receiveLoginEvent(self, err_code):
+    def _create_kiwoom_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    #발생한 이벤트를 처리할 함수를 맵핑해주는 함수.
+    def _set_signal_slots(self):
+        self.OnEventConnect.connect(self._event_connect)
+        self.OnReceiveTrData.connect(self._event_receive_tr_data)
+
+    # 로그인 요청 후 결과 이벤트를 받을 수 있는 루프를 생성해주는 함수.
+    def comm_connect(self):
+        self.dynamicCall("CommConnect()")       # 로그인창을 띄우는 함수 호출
+        self.login_event_loop = QEventLoop()    # QEventLoop는 키움증권 서버에서 보내오는 이벤트를 모두 처리할 수 있는듯 하다.
+        self.login_event_loop.exec_()           # 여기서는 OnEventConnect를 감시하고자 한다.
+
+    def _event_connect(self, err_code):
         if err_code == 0:
-            print("로그인 성공")
+            print("connected")
+        else:
+            print("disconnected")
 
-            try:
-                # 데이터 베이스를 연결하고 db를 선택해줌
-                conn = psycopg2.connect(conn_string)
-                print("연결성공")
+        self.login_event_loop.exit()            # 사용되어 필요없는 이벤트 루프를 종료한다.
 
-                cursor = conn.cursor()
+    def get_code_list_by_market(self, market):
+        code_list = self.dynamicCall("GetCodeListByMarket(QString)", market)
+        code_list = code_list.split(';')
+        return code_list[:-1]
 
-                # 스키마 생성
-                cursor.execute("create SCHEMA IF NOT EXISTS stock_kospi;")
-                cursor.execute("create SCHEMA IF NOT EXISTS stock_kosdaq;")
+    def get_master_code_name(self, code):
+        code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
+        return code_name
 
-                # 존재할 수 있는 테이블 제거
-                cursor.execute("drop TABLE IF EXISTS stock_kospi.daily_price;")
-                cursor.execute("drop TABLE IF EXISTS stock_kosdaq.daily_price;")
+    def set_input_value(self, id, value):
+        self.dynamicCall("SetInputValue(QString, QString)", id, value)
 
-                # stock_code스키마 하위에 kospi, kosdaq 테이블을 생성
-                cursor.execute("create TABLE stock_kospi.daily_price(code varchar(6) NOT NULL REFERENCES stock_code.kospi(code), date varchar(20) NOT NULL, open integer, low integer, high integer, close integer, volumeCount integer, volumeMoney integer, PRIMARY KEY(code, date));")
-                cursor.execute("create TABLE stock_kosdaq.daily_price(code varchar(6) NOT NULL REFERENCES stock_code.kosdaq(code), date varchar(20) NOT NULL, open integer, low integer, high integer, close integer, volumeCount integer, volumeMoney integer, PRIMARY KEY(code, date));")
+    # 데이터에서 하나씩 뽑아서 반환해주는 함수.
+    def _comm_get_data(self, code, real_type, field_name, index, item_name):
+        ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString", code, real_type, field_name, index, item_name)
+        return ret.strip()
 
-                # stock_code.kospi 테이블의 레코드를 모두 가져옴
-                cursor.execute("select * FROM stock_code.kospi;")
-                self.code_name_list = cursor.fetchall()  # type : 튜플로 이루어진 list
+    # 요청을 보낸뒤 결과 이벤트를 받을 수 있는 루프를 생성해주는 함수.
+    def comm_rq_data(self, rqname, trcode, next, screen_no):
+        self.dynamicCall("CommRqData(QString, QString, int, QString", rqname, trcode, next, screen_no)  # 서버에 데이터를 요청한다.
+        self.tr_event_loop = QEventLoop()   # QEventLoop는 키움증권 서버에서 보내오는 이벤트를 모두 처리할 수 있는듯 하다.
+        self.tr_event_loop.exec_()          # 여기서는 OnReceiveTrData 이벤트를 감시하고자 한다.
 
+    # OnReceiveTrData이벤트가 발생하면 불리는 함수.
+    def _event_receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
+        if next == '2':
+            self.remained_data = True
+        else:
+            self.remained_data = False
 
-            except Exception as e:
-                print("error")
-                print(e)
-
-            conn.commit()  # 이게 없으면 실제로 반영이 안됨.
-            cursor.close()
-            conn.close()
-
-            self.requestTrData(0)  # 로그인 성공이 확인된 후에만 요청을 할 수 있다.
-
-    def requestTrData(self, c):
-            self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", self.code_name_list[self.code_name_list_currentIndex][0])  # 종목코드
-            self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "기준일자", "20180508")  # 조회할 날짜
-            self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "0")   # 0 or 1, 1: 유상증자, 2: 무상증자, 4: 배당락, 8: 액면불할, 16: 액면병합, 32: 기업합병, 64: 감자, 256: 권리락
-            self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10081_req", "opt10081", c, "0766") # 사용사요청명칭 / 요청함수 / 초기조회:0, 연속조회:2 / 화면번호
-
-    def receiveTrData(self, screen_no, rqname, trcode, recordname, prev_next, data_len, err_code, msg1, msg2):
+        # 받은 데이터를 하나씩 읽는 함수에 전달
+        # 여러 종류의 TR을 요청해도 모두 이 메서드 내에서 처리해야한다.
         if rqname == "opt10081_req":
-            try:
-                # 데이터 베이스를 연결하고 db를 선택해줌
-                conn = psycopg2.connect(conn_string)
-                print("연결성공")
+            self._opt10081(rqname, trcode)
+        '''
+        if  rqname == "opt10082_req":
+            self._opt10082(rqname, trcode)
+        '''
 
-                cursor = conn.cursor()
+        try:
+            self.tr_event_loop.exit()       # 사용되어 필요없는 이벤트 루프를 종료한다.
+        except AttributeError:
+            pass
 
-                print(prev_next)
-                for i in range(800): # 한번 요청에 600일의 정보가 들어오는데 혹시몰라서 큰 값으로 잡음.
-                    date = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "일자")
+    #받아온 데이터를 한개씩 읽는 함수.
+    def _opt10081(self, rqname, trcode):
+        # ex) rqname : "opt10081_req"
+        # ex) trcode : "opt10081"
+        data_cnt = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)      # 데이터를 받을 때 몇개의 데이터가 왔는지 반환해주는 함수.
 
-                    if (date == ""):
-                        break
-                    startPrice = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "시가")
-                    highPrice = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "고가")
-                    lowPrice = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "저가")
-                    closePrice = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "현재가")
-                    volumeCount = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "거래량")
-                    volumeMoney = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, i, "거래대금")
+        try:
+            conn = psycopg2.connect(conn_string)
+            print("연결성공")
+            cursor = conn.cursor()
 
-                    print(i, date.strip(), "\t", startPrice.strip(), "\t", highPrice.strip(), "\t", lowPrice.strip(), "\t", closePrice.strip(), "\t", volumeCount.strip(), "\t", volumeMoney.strip())
+            # stock_code.kospi 테이블의 레코드를 모두 가져옴
+            #cursor.execute("select * FROM stock_code." + MODE + ";")
+            #self.code_name_list = cursor.fetchall()  # type : 튜플로 이루어진 list
 
-                # 레코드 삽입                                                                                                                                             #date + "\');
-                cursor.execute("insert INTO stock_kospi.daily_price(code, date, open, low, high, close, volumeCount, volumeMoney) VALUES(\'" + self.code_name_list[self.code_name_list_currentIndex][0] + "\',\'" + date + "\',\'" + startPrice + "\',\'" + lowPrice + "\',\'" + highPrice + "\',\'" + closePrice + "\',\'" + volumeCount + "\',\'" + volumeMoney + "\');")
+            for i in range(data_cnt):
+                date = self._comm_get_data(trcode, "", rqname, i, "일자")
+                open = self._comm_get_data(trcode, "", rqname, i, "시가")
+                high = self._comm_get_data(trcode, "", rqname, i, "고가")
+                low = self._comm_get_data(trcode, "", rqname, i, "저가")
+                close = self._comm_get_data(trcode, "", rqname, i, "현재가")
+                volume = self._comm_get_data(trcode, "", rqname, i, "거래량")
+                money = self._comm_get_data(trcode, "", rqname, i, "거래대금")
 
+                print(date, open, high, low, close, volume, money)
+                cursor.execute("insert INTO stock_kospi.daily_price(code, date, open, low, high, close, count, money) VALUES(\'" + self.req_stock_code + "\',\'" + date + "\',\'" + open + "\',\'" + high + "\',\'" + low + "\',\'" + close + "\',\'" + volume + "\',\'" + money + "\');")
 
-            except Exception as e:
-                print("error")
-                print(e)
+        except Exception as e:
+            print("error")
+            print(e)
 
+        conn.commit()  # 이게 없으면 실제로 반영이 안됨.
+        cursor.close()
+        conn.close()
 
-            if (prev_next == '2'):
-                print("연속조회")
-                time.sleep(0.5)        # !이 딜레이를 없애면 초당 요청 블락에 걸려서 전체 조회가 되지 않는다.!
-                self.requestTrData(2)   #연속조회
-            else:
-                conn.commit()  # 이게 없으면 실제로 반영이 안됨.
-                cursor.close()
-                conn.close()
+    #넣어야할 데이터를 모두 넣었다면 stock_code.kospi 또는 stock_code.kosdaq의 isUpdated column값을 true로 세트해준다.
+    def check_Filled(self):
+        try:
+            conn = psycopg2.connect(conn_string)
+            print("연결성공")
+            cursor = conn.cursor()
+            cursor.execute("update stock_code."+ MODE + " SET isupdated = true WHERE code = \'" + self.req_stock_code + "\';")
+        except Exception as e:
+            print("error")
+            print(e)
 
-                print("조회완료")
-                self.code_name_list_currentIndex = self.code_name_list_currentIndex + 1
-
-                if(len(self.code_name_list) < self.code_name_list_currentIndex):
-                    self.requestTrData(0)
-                #여기서 리퀘스트 0을 다시 부른다.
+        conn.commit()  # 이게 없으면 실제로 반영이 안됨.
+        cursor.close()
+        conn.close()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    trace = Program()
-    app.exec_()
+    kiwoom = Kiwoom()
+    kiwoom.comm_connect()
+
+
+    #!!!데이터베이스 컨넥스 객체와 컨객체를  생성자에서 만들어주고 정상정으로 완전히 입력될 때만 chek.Filled()함수를 부르고 commit을 진행하면 좋을거 같다.
+
+    # opt10081 TR 요청
+    kiwoom.req_stock_code = "039490"
+    kiwoom.set_input_value("종목코드", kiwoom.req_stock_code)
+    kiwoom.set_input_value("기준일자", "20170224")
+    kiwoom.set_input_value("수정주가구분", 1)
+    kiwoom.comm_rq_data("opt10081_req", "opt10081", 0, "0101")      # 세번째인자가 0일때 한번조회
+
+    while kiwoom.remained_data == True:
+        time.sleep(TR_REQ_TIME_INTERVAL)
+        kiwoom.set_input_value("종목코드", kiwoom.req_stock_code)
+        kiwoom.set_input_value("기준일자", "20170224")
+        kiwoom.set_input_value("수정주가구분", 1)
+        kiwoom.comm_rq_data("opt10081_req", "opt10081", 2, "0101")  # 세번째인자가 2일때 연속조회
+
+    kiwoom.check_Filled()   # 정상적으로 다 입력한 테이블은 isupdated column을 true로 수정해준다.
+
+    print("연속조회 완료")
